4f22129ae8a93ea0d54e0c5760bc614a698a1d8f
mee96 <carmenmc96@gmail.com>
2025-10-09 06:52:58 +0200

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+
+import "../scss/App.scss";
+
+function App() {
+  return (
+      <h1> hello world</h1>
+    
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+import { Link } from 'react-scroll';
+
+function Header() {
+  return (
+    <header className="header">
+               
+
+
+    </header>
+  )
+}
+
+export default Header
