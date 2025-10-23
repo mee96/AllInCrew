3202bc5e4fa60a40429290076a98097f60d04447
mee96 <carmenmc96@gmail.com>
2025-10-23 16:51:39 +0200

    act header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
+import "../scss/layout/Header.scss";
+import logoPortf from "../images/1.png";
+
+
 function Header() {
     return (
-        <header>
-            <h1>Welcome to AllinCrew</h1>
+        <header className="header">
+            <img className="header__logo" src={logoPortf} alt="logo" />
+            <h1>AllinCrew tus patronas </h1>
+           
+           < ul className="header__list">
+            <li>Quienes somos</li>
+            <li>Covers</li>
+            <li>Fotos</li>
+            <li>Blogs</li>
+           </ul>
         </header>
     );
 }
